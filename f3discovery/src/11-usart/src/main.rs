2665a5bdc6b968d5721f2c6b45ec95644e40a0e3
Antal Spector-Zabusky <antal.b.sz@gmail.com>
2024-01-27 13:56:22 -0500

    Ch. 11: How to throttle serial

--- f3discovery/src/11-usart/src/main.rs
+++ f3discovery/src/11-usart/src/main.rs
@@ -6,14 +6,23 @@ use aux11::{entry, iprint, iprintln};
 
 #[entry]
 fn main() -> ! {
-    let (usart1, _mono_timer, _itm) = aux11::init();
+    let (usart1, mono_timer, mut itm) = aux11::init();
 
-    // Send a single character
+    let instant = mono_timer.now();
+    // Send a string
     b"The quick brown fox jumps over the lazy dog.".iter().for_each(|c| {
-        usart1
-            .tdr
-            .write(|w| w.tdr().bits(u16::from(*c)) );
+        while usart1.isr.read().txe().bit_is_clear() {}
+        
+        usart1.tdr.write(|w| w.tdr().bits(u16::from(*c)));
     });
+    let elapsed = instant.elapsed(); // in ticks
+
+    iprintln!(
+        &mut itm.stim[0],
+        "`for` loop took {} ticks ({} us)",
+        elapsed,
+        elapsed as f32 / mono_timer.frequency().0 as f32 * 1e6
+    );
 
     loop {}
 }
