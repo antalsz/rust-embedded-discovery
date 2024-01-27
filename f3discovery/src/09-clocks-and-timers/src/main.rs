3678fe543d7f5fa30273b318d9a6789a01656c8e
Antal Spector-Zabusky <antal.b.sz@gmail.com>
2024-01-27 04:11:49 -0500

    103,000 loops/second?  That seems… low?

--- f3discovery/src/09-clocks-and-timers/src/main.rs
+++ f3discovery/src/09-clocks-and-timers/src/main.rs
@@ -5,7 +5,23 @@ use aux9::{entry, switch_hal::OutputSwitch, tim6};
 
 #[inline(never)]
 fn delay(tim6: &tim6::RegisterBlock, ms: u16) {
-    // TODO implement this
+    // Processor is 72MHz + most instructions are 1 tick
+    // Guess: loop is 2 instructions
+    // Thus, computation:
+    //     1 instr * 72 MHz = 72×10⁶ instr/sec
+    //     72×10⁶ instr/sec / 2 instr/loop = 72×10⁶/2 instr/loop
+    // Or, in other words: running at 72MHz means 72M instructions for 1 second,
+    // so obviously that means half that many loops if a loop is 2 instructions.
+    // Duh.
+    
+    for _ in 0..103_000 {}
+
+    // Ok, I was very wrong.
+    //
+    //     X instr/loop = 72×10⁶ instr/sec / 103×10³ loop/sec
+    //                  ≈ 699 instr/loop
+    //
+    // Really‽
 }
 
 #[entry]
