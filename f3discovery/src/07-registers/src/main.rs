b178f13f2e842ad21fcb9bef820d0e99e64876fe
Antal Spector-Zabusky <antal.b.sz@gmail.com>
2024-01-27 03:15:18 -0500

    Chapter 7: Move the fun blinky lights out of the way

--- f3discovery/src/07-registers/src/main.rs
+++ f3discovery/src/07-registers/src/main.rs
@@ -6,13 +6,10 @@ use core::ptr;
 #[allow(unused_imports)]
 use aux7::{entry, iprint, iprintln};
 
-fn off_on(off_on: u8) -> u32 {
-    ((!off_on) as u32) << (8+16) | (off_on as u32) << 8
-}
-
-#[entry]
-fn main() -> ! {
-    let (mut _itm, _) = aux7::init();
+fn _fun_blink_steps(itm: &mut aux7::ITM) -> () {
+    fn off_on(off_on: u8) -> u32 {
+        ((!off_on) as u32) << (8+16) | (off_on as u32) << 8
+    }
 
     unsafe {
         // A magic address!
@@ -20,7 +17,7 @@ fn main() -> ! {
 
         let mut set_leds = |pattern: u8| {
             ptr::write_volatile(GPIOE_BSRR as *mut u32, off_on(pattern));
-            iprintln!(&mut _itm.stim[0], "{:#010b}", pattern);
+            iprintln!(&mut itm.stim[0], "{:#010b}", pattern);
         };
 
         for i in 0..8 {
@@ -31,6 +28,12 @@ fn main() -> ! {
             set_leds(pattern);
         }
     }
+}
+
+#[entry]
+fn main() -> ! {
+    let (mut _itm, _) = aux7::init();
+    // _fun_blink_steps(&mut _itm);
 
     loop {}
 }
