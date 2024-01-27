ef22d3059d5265ee63dffa5832759d530c6e9944
Antal Spector-Zabusky <antal.b.sz@gmail.com>
2024-01-27 13:08:46 -0500

    Update to their code

--- f3discovery/src/09-clocks-and-timers/src/main.rs
+++ f3discovery/src/09-clocks-and-timers/src/main.rs
@@ -5,16 +5,6 @@ use aux9::{entry, switch_hal::OutputSwitch, tim6};
 
 #[inline(never)]
 fn delay(tim6: &tim6::RegisterBlock, ms: u16) {
-    // Clock frequency is 8 MHz / (psc + 1)
-    // Goal: Clock frequency is 1/ms = 1/(10⁻³ Hz) = 1 kHz
-    // 1 kHz = 8 MHz / (psc + 1)
-    // (psc + 1) = 8 MHz / 1 kHz = 8×10⁶ Hz / 1×10³ Hz = 8×10³ = 8,000
-    // psc = 7,999
-
-    // The one thing I missed is that you should disable the counter first in
-    // case somebody else left it running.
-    tim6.cr1.write(|w| w.opm().set_bit().cen().clear_bit());
-    tim6.psc.write(|w| w.psc().bits(7_999));
     tim6.arr.write(|w| w.arr().bits(ms));
     tim6.cr1.write(|w| w.cen().set_bit());
     while tim6.sr.read().uif().is_clear() { }
@@ -26,7 +16,19 @@ fn main() -> ! {
     let (leds, rcc, tim6) = aux9::init();
     let mut leds = leds.into_array();
 
+    // Clock frequency is 8 MHz / (psc + 1)
+    // Goal: Clock frequency is 1/ms = 1/(10⁻³ Hz) = 1 kHz
+    // 1 kHz = 8 MHz / (psc + 1)
+    // (psc + 1) = 8 MHz / 1 kHz = 8×10⁶ Hz / 1×10³ Hz = 8×10³ = 8,000
+    // psc = 7,999
+
+    // Their code configures `tim6` for everybody all at once at the start, but
+    // is otherwise the same.  Makes sense!
     rcc.apb1enr.write(|w| w.tim6en().set_bit());
+    // The one thing I missed is that you should disable the counter first in
+    // case somebody else left it running.
+    tim6.cr1.write(|w| w.opm().set_bit().cen().clear_bit());
+    tim6.psc.write(|w| w.psc().bits(7_999));
 
     let ms = 1000;
     loop {
