ad8c630a86a2b856131eb303d7e7b80035793550
Antal Spector-Zabusky <antal.b.sz@gmail.com>
2024-01-28 03:09:32 -0500

    Chapter 11: Line-reverser

--- f3discovery/src/11-usart/src/main.rs
+++ f3discovery/src/11-usart/src/main.rs
@@ -6,6 +6,7 @@ use core::fmt::{self, Write};
 
 #[allow(unused_imports)]
 use aux11::{entry, iprint, iprintln, usart1};
+use heapless::Vec;
 
 macro_rules! uprint {
     ($serial:expr, $($arg:tt)*) => {
@@ -26,12 +27,23 @@ struct SerialPort {
     usart1: &'static mut usart1::RegisterBlock,
 }
 
+impl SerialPort {
+    fn read_byte(&mut self) -> u8 {
+        while self.usart1.isr.read().rxne().bit_is_clear() {}
+        self.usart1.rdr.read().rdr().bits() as u8
+    }
+
+    fn write_bytes(&mut self, bytes: impl Iterator<Item = u8>) -> () {
+        bytes.for_each(|byte| {
+            while self.usart1.isr.read().txe().bit_is_clear() {}
+            self.usart1.tdr.write(|w| w.tdr().bits(byte as u16));
+        })
+    }
+}
+
 impl fmt::Write for SerialPort {
     fn write_str(&mut self, s: &str) -> fmt::Result {
-        s.bytes().for_each(|c| {
-            while self.usart1.isr.read().txe().bit_is_clear() {};
-            self.usart1.tdr.write(|w| w.tdr().bits(u16::from(c)))
-        });
+        self.write_bytes(s.bytes());
         Ok(())
     }
 }
@@ -40,10 +52,24 @@ impl fmt::Write for SerialPort {
 fn main() -> ! {
     let (usart1, _mono_timer, _itm) = aux11::init();
 
+    let mut serial = SerialPort { usart1 };
+
+    // A buffer with 32 bytes of capacity
+    let mut buffer: Vec<u8, 32> = Vec::new();
+
     loop {
-        while usart1.isr.read().rxne().bit_is_clear() {}
-        let value = usart1.rdr.read().rdr().bits();
-        while usart1.isr.read().txe().bit_is_clear() {};
-        usart1.tdr.write(|w| w.tdr().bits(value));
+        match serial.read_byte() {
+            b'\r' => {
+                serial.write_bytes(buffer.iter().rev().cloned());
+                uprintln!(serial, "");
+                buffer.clear();
+            },
+            byte => match buffer.push(byte) {
+                Ok(()) => {},
+                Err(lost) => {
+                    uprintln!(serial, "LOST DATA: {}", lost as char);
+                }
+            }
+        }
     }
 }
